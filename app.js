69779e7ca3caabbbbd916b80bfb95a5df6c95d49
Torjus <Torjus24sfj@gmail.com>
2025-01-15 10:47:22 +0100

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+const gameBoard = document.querySelector("#gameboard")
+const playerDisplay = document.querySelectorAll("#player")
+const infoDisplay = document.querySelectorAll("#info-display")
+const width = 8
+
+const startPieces = [
+    
+]
+
