ae6f7edb1779cbed5e97dbe6f35de366900bd06e
Torjus <Torjus24sfj@gmail.com>
2025-01-15 12:16:18 +0100

    math

--- app.js
+++ app.js
@@ -4,6 +4,33 @@ const infoDisplay = document.querySelectorAll("#info-display")
 const width = 8
 
 const startPieces = [
-    
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
 
+function createBoard(){
+    startPieces.forEach((startPieces, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML = startPieces
+        square.setAttribute('square-id', i)
+        // square.classList.add('light')
+        const row = Math.floor(i / width)
+        if ( (row + i ) % 2 === 0){
+            square.classList.add("light")
+        }
+            else 
+            {
+                square.classList.add("dark")
+            }
+       
+        gameBoard.append(square)
+    })
+}
+createBoard();
